57254a15c007632da92c2cddb4edba325775bbbc
Mirradell <olya.shtejn@yandex.ru>
2021-05-26 10:57:49 +0300

    fixed some errors

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as QA from './QuestionsAnswers.js';
 const http = require('http');
 const express = require('express');
 const socketIO  = require('socket.io');
-const port = process.env.PORT || 5000;
+export const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.static(`${__dirname}/client`));
 
