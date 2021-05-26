167164eacfbaca9bbcacda0116f3e2b6b960f5f9
Mirradell <olya.shtejn@yandex.ru>
2021-05-26 10:03:04 +0300

    fixed some errors

--- client/src/player.js
+++
@@ -1,9 +0,0 @@
-// const canvasPlayer = document.getElementById("playersCanvas");
-// const contextPlayer = canvasPlayer.getContext("2d");
-//
-// const drawPlayer = () => {
-//     contextPlayer.fillStyle = "red";
-//     contextPlayer.fillRect(0, 0, 5, 5);
-// }
-//
-// drawPlayer();
---
+++ main.js
@@ -0,0 +1,27 @@
+import * as QA from './QuestionsAnswers.js';
+
+const http = require('http');
+const express = require('express');
+const socketIO  = require('socket.io');
+
+const app = express();
+app.use(express.static(`${__dirname}/client`));
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    socket.on('token',  QA.qToken(socket));
+    socket.on('start',  QA.qStart(socket));
+    socket.on('turn',   QA.qTurn(socket));
+    socket.on('finish', QA.qFinish(socket));
+    socket.on('disconnect', QA.qDisconnect(socket));
+});
+
+server.on('error', (error) => {
+    console.error(error);
+});
+
+server.listen(8080, () => {
+   console.log('server is listening...');
+});
